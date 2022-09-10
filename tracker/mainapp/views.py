4a9182bd06bc3e19df7267894b0eb2f380e08342
piyushrj100 <piyush1.bksc@gmail.com>
2022-09-11 00:54:14 +0530

    continued

--- tracker/mainapp/views.py
+++ tracker/mainapp/views.py
@@ -1,7 +1,9 @@
 from http.client import HTTPResponse
+from threading import Thread
 from django.shortcuts import render
 from yahoo_fin.stock_info import * 
 import time 
+import queue
 
 
 # Create your views here.
@@ -23,17 +25,27 @@ def stockTracker(request) :
             return HTTPResponse("Error")
     n_threads=len(stockpicker)
     thread_list=[]
+    que = queue.Queue()
 
     start=time.time()
     print(time)
 
     # for i in stockpicker : 
-    #     details = get_quote_table(i)
-    #     data.update({i:details})
+    #     result = get_quote_table(i)
+    #     data.update({i:result})
     for i in range(n_threads) :
-        thread = Thread(target = lambda q, args1: q.put({stockpicker[i] : get_quote_table(arg1)}), args=())
+        thread = Thread(target = lambda q, arg1: q.put({stockpicker[i] : get_quote_table(arg1)}), args=(que, stockpicker[i]))
+        thread_list.append(thread)
+        thread_list[i].start()
+    for thread in thread_list :
+        thread.join()
+    
+    while not que.empty() :
+        result = que.get()
+        data.update(result)
+        
     end=time.time()
     time_taken= end-start
     print(time_taken)
     print(data)
-    return render(request, 'mainapp/stocktracker.html')
+    return render(request, 'mainapp/stocktracker.html', {'data' : data})
