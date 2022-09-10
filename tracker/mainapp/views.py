4563c41647a5d92abc3d3715dcdcf5c37c2d77ad
piyushrj100 <piyush1.bksc@gmail.com>
2022-09-10 23:20:14 +0530

    continued

--- tracker/mainapp/views.py
+++ tracker/mainapp/views.py
@@ -1,9 +1,39 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from yahoo_fin.stock_info import * 
+import time 
+
 
 # Create your views here.
 
 def stockPicker(request) :
-    return render(request,'mainapp/stockpicker.html')
+    stock_picker = tickers_nifty50()
+    print(stock_picker)
+    return render(request,'mainapp/stockpicker.html',{'stockpicker' : stock_picker})
 
 def stockTracker(request) :
+    stockpicker = request.GET.getlist('stockpicker')
+    print(stockpicker)
+    data = {}
+    available_stocks = tickers_nifty50()
+    for i in stockpicker : 
+        if i in available_stocks : 
+            pass 
+        else : 
+            return HTTPResponse("Error")
+    n_threads=len(stockpicker)
+    thread_list=[]
+
+    start=time.time()
+    print(time)
+
+    # for i in stockpicker : 
+    #     details = get_quote_table(i)
+    #     data.update({i:details})
+    for i in range(n_threads) :
+        thread = Thread(target = lambda q, args1: q.put({stockpicker[i] : get_quote_table(arg1)}), args=())
+    end=time.time()
+    time_taken= end-start
+    print(time_taken)
+    print(data)
     return render(request, 'mainapp/stocktracker.html')
